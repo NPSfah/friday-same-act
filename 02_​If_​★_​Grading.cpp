91c709a72cc4b0b58fa332977235c11c7e082b51
Hattakorn Hattakarn <162902848+Karnhh@users.noreply.github.com>
2024-09-01 22:34:03 +0700

    Create 02_​If_​★_​Grading.cpp

---
+++ 02_​If_​★_​Grading.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    double s;
+    cin>>s;
+    if(s>=80)
+    {
+        cout<<"A";
+    }else if(s>=70)
+    {
+        cout<<"B";
+    }else if(s>=60)
+    {
+        cout<<"C";
+    }else if(s>=50)
+    {
+        cout<<"D";
+    }else{
+        cout<<"F";
+    }
+    return 0;
+}
