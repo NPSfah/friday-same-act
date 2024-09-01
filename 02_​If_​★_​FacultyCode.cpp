feadab448e3e71aa69b88158949bef4e472161a9
Hattakorn Hattakarn <162902848+Karnhh@users.noreply.github.com>
2024-09-01 22:33:24 +0700

    Create 02_​If_​★_​FacultyCode.cpp

---
+++ 02_​If_​★_​FacultyCode.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    string code;
+    cin>>code;
+    if(code == "01" || code == "02" || code == "20" || code == "21" || code == "22" || code == "23" || code == "24" || code == "25" || code == "26" || code == "27" || code == "28" || code == "29" || code == "30" || code == "31" || code == "32" || code == "33" || code == "34" || code == "35" || code == "36" || code == "37" || code == "38" || code == "39" || code == "40" || code == "51" || code == "53" || code == "55" || code == "58")
+    {
+        cout<<"OK";
+    }else{
+        cout<<"Error";
+    }
+    return 0;
+}
