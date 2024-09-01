5a9da8f97648c2dcf2cb574470d6a2e89c6dd8f7
Hattakorn Hattakarn <162902848+Karnhh@users.noreply.github.com>
2024-09-01 22:22:25 +0700

    Delete 01_​Expr_​★_​An_​Expression.cpp

--- 01_​Expr_​★_​An_​Expression.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main ()
-{
-    cout<<(M_PI-(10*9*8*7*6*5*4*3*2*1/pow(8,8))+pow(log(9.7),(7/sqrt(71))-sin(2*M_PI/9)))/pow(1.2,cbrt(2.3));
-    return 0;
-}
