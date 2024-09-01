e19e256fd01b2a46ca94e71f376bd68bcbdb7ddd
Hattakorn Hattakarn <162902848+Karnhh@users.noreply.github.com>
2024-09-01 22:29:44 +0700

    Create 01_​Expr_​★★_​Duration.cpp

---
+++ 01_​Expr_​★★_​Duration.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int h1,m1,s1, h2,m2,s2;
+    cin >> h1 >> m1 >> s1;
+    cin >> h2 >> m2 >> s2;
+    int t1 = h1*60*60 + m1*60 + s1;
+    int t2 = h2*60*60 + m2*60 + s2;
+    int dh,dm,ds;
+    t2 += (24*60*60);
+    ds = t2 - t1;
+    ds %= (24*60*60);
+    dh = ds / (60*60);
+    ds -= dh * 60*60;
+    dm = ds / 60;
+    ds -= dm*60;
+    cout << dh << ':' << dm << ':' << ds;
+}
